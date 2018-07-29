347a177bc0c0fe5bf3ba6f985613c557d9bb6e39
Saurabh S Bagalkar <sbagalka@asu.edu>
2018-07-28 19:25:48 -0500

    HSV values

--- tech_challenge/identify_slide.py
+++ tech_challenge/identify_slide.py
@@ -42,7 +42,30 @@ for current_img in a_data:
 	image_complement = cv2.bitwise_not(img_bw)
 	repmat_binary = np.repeat(image_complement[:, :, np.newaxis], 3, axis=2)
 	rgb_zeroed = cv2.bitwise_and(current_img, repmat_binary)
-	cv2.imshow('B', rgb_zeroed)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	hsv = cv2.cvtColor(rgb_zeroed, cv2.COLOR_BGR2HSV)
+	hue=hsv[:,:,0].flatten()
+	saturation = hsv[:,:,1].flatten()
+	value = hsv[:,:,2].flatten()
+	mean_hue = (np.true_divide(hue.sum(0), (hue != 0).sum(0)))*2
+	mean_sat = (np.true_divide(saturation.sum(0), (saturation != 0).sum(0)))/255
+	mean_value = (np.true_divide(value.sum(0), (value != 0).sum(0)))/255
+	a_hue.append(mean_hue)
+	a_sat.append(mean_sat)
+	a_value.append(mean_value)
 	
+for current_img in a_data:
+	gray_image = cv2.cvtColor(current_img, cv2.COLOR_BGR2GRAY)
+	ret,img_bw = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	image_complement = cv2.bitwise_not(img_bw)
+	repmat_binary = np.repeat(image_complement[:, :, np.newaxis], 3, axis=2)
+	rgb_zeroed = cv2.bitwise_and(current_img, repmat_binary)
+	hsv = cv2.cvtColor(rgb_zeroed, cv2.COLOR_BGR2HSV)
+	hue=hsv[:,:,0].flatten()
+	saturation = hsv[:,:,1].flatten()
+	value = hsv[:,:,2].flatten()
+	mean_hue = (np.true_divide(hue.sum(0), (hue != 0).sum(0)))*2
+	mean_sat = (np.true_divide(hue.sum(0), (hue != 0).sum(0)))/255
+	mean_value = (np.true_divide(hue.sum(0), (hue != 0).sum(0)))/255
+	a_hue.append(mean_hue)
+	a_sat.append(mean_sat)
+	a_value.append(mean_value)
